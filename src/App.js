7216ab2f13a4c856b04c985e6d063d3d50d128ea
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-07-08 15:50:20 +0200

    feat: PartNERD starting project 01

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './stars-background.css';
+
+function App() {
+  return (
+    <div>
+      <div id="stars"></div>
+<div id="stars2"></div>
+<div id="stars3"></div>
+<div id="title">
+  <span>
+    PURE CSS
+  </span>
+  <br />
+  <span>
+    PARALLAX PIXEL STARS
+  </span>
+</div>
+    </div>
+  );
+}
+
+export default App;
