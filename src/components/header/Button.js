05c9db5016450f613c8b6b114770cf2441682d53
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-07-09 17:19:05 +0200

    feat: header 01

---
+++ src/components/header/Button.js
@@ -0,0 +1,10 @@
+import { Fragment } from 'react';
+
+import './button.css'
+
+const Button = (props) => {
+    return (
+<Fragment><div className='button'>{props.children}</div></Fragment>
+    )
+}
+export default Button;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Spinner from "../spinner/Spinner";
 import "./header.css";
 import "../spinner/spinner.css";
+import Button from './Button';
 
 const Header = () => {
   const [isActive, setIsActive] = useState(true);
@@ -14,11 +15,18 @@ const Header = () => {
 
   return (
     <React.Fragment>
-    <header>
-        <div>
-            PartNERD
-        </div>
-    </header>
+      <header style={{ opacity: !isActive ? 1 : 0, transition: '1.5s'}}>
+        {!isActive && (
+          <div className='container'>
+            <div>PartNERD</div>
+            <div className='buttons_right'>
+            <Button>Button 1</Button>
+            <Button>Button 2</Button>
+          </div>
+          </div>
+        )}
+      </header>
+
       <div
         style={{ opacity: !isActive ? 0 : 1, transition: "1.5s" }}
         className="title"
