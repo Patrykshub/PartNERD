c20e167713aca97e6c778661e61ceffc5a4f7d2b
Patrykshub <63500126+Patrykshub@users.noreply.github.com>
2021-07-09 16:10:05 +0200

    feat: intro done

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Spinner from "../spinner/Spinner";
 import "./header.css";
+import "../spinner/spinner.css";
 
 const Header = () => {
+  const [isActive, setIsActive] = useState(true);
+
+  const spinnerHandler = () => {
+    setIsActive(false);
+    console.log("spinner deactivated");
+  };
+
   return (
     <React.Fragment>
-      <header className='header'>
-      </header>
-      <Spinner />
+        <div style={{opacity: !isActive ? 0 : 1, transform: 'all 5s'}} className="title">
+          <span onClick={spinnerHandler}>PartNERD</span>
+        </div>
+        <div style={{opacity: !isActive ? 0 : 1, transform: 'all 5s'}}>
+        <Spinner />
+        </div>
     </React.Fragment>
   );
 };
--- src/stars-background.js
+++ src/stars-background.js
@@ -5,9 +5,6 @@ import './stars-background.css';
 const StarsBackground = () => {
   return (
     <div>
-    <div className='title'>
-    <span>PartNERD</span>
-    </div>
       <div className='stars'></div>
       <div className='stars2'></div>
       <div className='stars3'></div>
